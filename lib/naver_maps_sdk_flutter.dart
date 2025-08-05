7ca1dca0e0b21859b87f519fdde7e67b6d721141
ParkGiTeak <rl980901@naver.com>
2025-08-05 16:03:58 +0900

    feat: NaverMapSDK Class 싱글톤 제작, initNaverMapSDK를 통해 htmlContent를 생성하게 처리.

--- lib/naver_maps_sdk_flutter.dart
+++ lib/naver_maps_sdk_flutter.dart
@@ -1,3 +1,56 @@
-class NaverMapSDK {
+import 'package:flutter/services.dart';
+import 'package:naver_maps_sdk_flutter/enum/naver_map_language_type.dart';
 
+class NaverMapSDK {
+  static NaverMapSDK? _instance;
+
+  NaverMapSDK._internal();
+
+  static NaverMapSDK get instance => _instance ??= NaverMapSDK._internal();
+
+  bool _isInitialized = false;
+  late final String _clientId;
+  late final NaverMapLanguageType _language;
+  late final String _htmlContent;
+
+  bool get isInitialized => _isInitialized;
+
+  String get clientId => _clientId;
+
+  NaverMapLanguageType get language => _language;
+
+  String get htmlContent {
+    if (!_isInitialized) {
+      throw Exception(
+        'NaverMapSDK is not initialized. Call NaverMapSDK.initialize() first.',
+      );
+    }
+    return _htmlContent;
+  }
+
+  static Future<void> initialize({
+    required String clientId,
+    NaverMapLanguageType language = NaverMapLanguageType.korean,
+  }) async {
+    if (instance.isInitialized) {
+      print('NaverMapSDK is already initialized.');
+      return;
+    }
+    instance._clientId = clientId;
+    instance._language = language;
+
+    final String beforeHtmlContent = await rootBundle.loadString(
+      'assets/naver_map.html',
+    );
+
+    final String scriptPlaceHolder =
+        '<script id="naverMapScript" type="text/javascript"></script>';
+    final String scriptNaverWithClientId =
+        '<script type="text/javascript" src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${instance.clientId}&language=${instance.language.value}"></script>';
+    instance._htmlContent = beforeHtmlContent.replaceFirst(
+      scriptPlaceHolder,
+      scriptNaverWithClientId,
+    );
+    instance._isInitialized = true;
+  }
 }
