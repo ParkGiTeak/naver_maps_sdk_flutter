f7fe9c21d9d7bbeaf8b5e8b2b0a3e520d5bd1be8
ParkGiTeak <rl980901@naver.com>
2025-08-29 14:19:56 +0900

    feat: NaverMapManager 클래스 용 Interface 추가

---
+++ lib/interface/naver_map_manager_interface.dart
@@ -0,0 +1,93 @@
+import '../model/bounds.dart';
+import '../model/coord.dart';
+import '../model/marker_options.dart';
+import '../model/n_lat_lng_bounds.dart';
+import '../model/n_point_bounds.dart';
+
+abstract interface class NaverMapManagerInterface {
+  void onMapLoadSuccess();
+
+  void onMapLoadFail();
+
+  void onMarkerClick(int markerId);
+
+  void onMapClick(Map<String, dynamic> coord);
+
+  void onMapLongTap(Map<String, dynamic> coord);
+
+  void onMapIdle();
+
+  void onMapZoomChanged(int zoom);
+
+  void onMapZoomEnd();
+
+  void onMapZoomStart();
+
+  void onMapCenterChanged(Map<String, dynamic> center);
+
+  void dispose();
+
+  Future<Coord> getCenter({required bool resultTypeLatLng});
+
+  Future<int> getZoom();
+
+  Future<Bounds> getBounds({required bool resultTypeLatLng});
+
+  Future<bool> hasLatLng({required NLatLngBounds bounds, required Coord coord});
+
+  Future<bool> hasPoint({required NPointBounds bounds, required Coord coord});
+
+  Future<void> setCenter({required Coord center});
+
+  Future<void> setZoom({required int zoom});
+
+  Future<void> addMarker({
+    required String markerId,
+    required MarkerOptions markerOptions,
+  });
+
+  Future<void> updateMarker({
+    required String markerId,
+    required MarkerOptions markerOptions,
+  });
+
+  Future<void> removeMarker({required String markerId});
+
+  Future<void> removeMarkerAll();
+
+  Future<List<int>> getMarkerIds();
+
+  Future<void> addMarkerClickEvent({required String markerId});
+
+  Future<void> removeMarkerClickEvent({required String markerId});
+
+  Future<void> addMapClickEventListener();
+
+  Future<void> removeMapClickEventListener();
+
+  Future<void> addMapLongTapEventListener();
+
+  Future<void> removeMapLongTapEventListener();
+
+  Future<void> addMapIdleEventListener();
+
+  Future<void> removeMapIdleEventListener();
+
+  Future<void> addMapZoomChangedEventListener();
+
+  Future<void> removeMapZoomChangedEventListener();
+
+  Future<void> addMapZoomEndEventListener();
+
+  Future<void> removeMapZoomEndEventListener();
+
+  Future<void> addMapZoomStartEventListener();
+
+  Future<void> removeMapZoomStartEventListener();
+
+  Future<void> addMapCenterChangedEventListener();
+
+  Future<void> removeMapCenterChangedEventListener();
+
+  Future<void> disposeMap();
+}
