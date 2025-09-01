4d1ff4573f5d4e338d752654a438ed0ae6a88b06
ParkGiTeak <rl980901@naver.com>
2025-09-01 20:48:23 +0900

    refactor: NaverMapWidget의 WebViewController 관련 설정 부분 NaverMapManager로 이동, NaverMapWidget App Class PageLoad전에 Loading 처리

--- lib/sdk_app/naver_map_manager_app.dart
+++ lib/sdk_app/naver_map_manager_app.dart
@@ -2,6 +2,10 @@ part of 'naver_maps_sdk_flutter_app.dart';
 
 class NaverMapManager implements NaverMapManagerInterface {
   final WebViewController _controller = WebViewController();
+  final Completer<void> _pageFinishedCompleter = Completer<void>();
+
+  late final String _mapDivId;
+  late final String _mapId;
 
   StreamController<MapLoadStatus>? _mapLoadStatusController;
   StreamController<MarkerEvent>? _markerEventController;
@@ -22,10 +26,128 @@ class NaverMapManager implements NaverMapManagerInterface {
     return _mapEventController!.stream;
   }
 
-  NaverMapManager._internal();
+  Future<void> get onPageFinished => _pageFinishedCompleter.future;
+
+  NaverMapManager._internal() {
+    _mapId = 'naver-map-$hashCode';
+    _mapDivId = 'naver-map-div-$hashCode';
+    _initializeWebViewController();
+  }
 
   static NaverMapManager createNaverMapManager() {
     return NaverMapManager._internal();
+  }
+
+  void _initializeWebViewController() async {
+    _controller
+      ..setOnConsoleMessage((message) {
+        debugPrint('WebView Console:: ${message.message}');
+      })
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('WebView is Loading Progress:: $progress');
+          },
+          onPageStarted: (String url) {
+            debugPrint('WebView Page Started:: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('WebView Page Finished:: $url');
+            if (!_pageFinishedCompleter.isCompleted) {
+              _pageFinishedCompleter.complete();
+            }
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('WebView Error:: ${error.description}');
+          },
+        ),
+      )
+      ..addJavaScriptChannel(
+        'MapLoadStatus_$_mapId',
+        onMessageReceived: (JavaScriptMessage message) {
+          debugPrint('WebView Message:: ${message.message}');
+          try {
+            final Map<String, dynamic> data = jsonDecode(message.message);
+            final String type = data['type'];
+
+            if (type == 'mapLoaded') {
+              onMapLoadSuccess();
+            } else if (type == 'mapLoadFail') {
+              onMapLoadFail();
+            }
+          } catch (e) {
+            onMapLoadFail();
+          }
+        },
+      )
+      ..addJavaScriptChannel(
+        'MapEvent_$_mapId',
+        onMessageReceived: (JavaScriptMessage message) {
+          final Map<String, dynamic> json = jsonDecode(message.message);
+          final String type = json['type'];
+          switch (type) {
+            case 'click':
+              onMapClick(json['coord']);
+              break;
+            case 'longtap':
+              onMapLongTap(json['coord']);
+              break;
+            case 'idle':
+              onMapIdle();
+              break;
+            case 'zoom_changed':
+              onMapZoomChanged(json['zoom']);
+              break;
+            case 'zoomend':
+              onMapZoomEnd();
+              break;
+            case 'zoomstart':
+              onMapZoomStart();
+              break;
+            case 'center_changed':
+              onMapCenterChanged(json['center']);
+              break;
+          }
+        },
+      )
+      ..addJavaScriptChannel(
+        'MarkerEvent_$_mapId',
+        onMessageReceived: (JavaScriptMessage message) {
+          final Map<String, dynamic> json = jsonDecode(message.message);
+          if (json['type'] == 'markerClick') {
+            onMarkerClick(json['markerId']);
+          }
+        },
+      )
+      ..loadHtmlString(
+        await _initNaverScript(),
+        baseUrl: NaverMapSDK.instance.webServiceUrl,
+      );
+  }
+
+  Future<String> _initNaverScript() async {
+    final String beforeHtmlContent = await rootBundle.loadString(
+      'packages/naver_maps_sdk_flutter/assets/naver_map.html',
+    );
+
+    final String scriptPlaceHolder =
+        '<script id="naverMapScript" type="text/javascript"></script>';
+    final String scriptNaverWithClientId =
+        '<script type="text/javascript" src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${NaverMapSDK.instance.clientId}&language=${NaverMapSDK.instance.language.value}"></script>';
+    final String mapDivPlaceHolder = 'id="map"';
+    final String mapDivId = 'id="$_mapDivId"';
+    return beforeHtmlContent
+        .replaceFirst(scriptPlaceHolder, scriptNaverWithClientId)
+        .replaceFirst(mapDivPlaceHolder, mapDivId);
+  }
+
+  Future<void> _initializeNaverMap(MapOptions? mapOptions) async {
+    debugPrint('mapOptions:: ${mapOptions?.toJson() ?? '{}'}');
+    await _controller.runJavaScript(
+      'initMap(${jsonEncode(_mapDivId)}, ${mapOptions?.toJson() ?? '{}'}, ${jsonEncode(_mapId)})',
+    );
   }
 
   @override
@@ -93,15 +215,17 @@ class NaverMapManager implements NaverMapManagerInterface {
   }
 
   @override
-  Future<Coord> getCenter({required bool resultTypeLatLng}) async {
+  Future<Coord> getCenter({required bool shouldReturnLatLng}) async {
     String jsonStringResult =
-        await _controller.runJavaScriptReturningResult('map.getCenter()')
+        await _controller.runJavaScriptReturningResult(
+              'window.getCenter(${jsonEncode(_mapId)})',
+            )
             as String;
     if (Platform.isIOS) {
       jsonStringResult = NSDictionaryUtil.convert(jsonStringResult);
     }
     final Map<String, dynamic> json = jsonDecode(jsonStringResult);
-    if (resultTypeLatLng) {
+    if (shouldReturnLatLng) {
       final Coord coord = NLatLng(json['_lat'], json['_lng']);
       return coord;
     } else {
@@ -113,21 +237,26 @@ class NaverMapManager implements NaverMapManagerInterface {
   @override
   Future<int> getZoom() async {
     final num jsonStringResult =
-        await _controller.runJavaScriptReturningResult('map.getZoom()') as num;
+        await _controller.runJavaScriptReturningResult(
+              'getZoom(${jsonEncode(_mapId)})',
+            )
+            as num;
     return jsonStringResult.toInt();
   }
 
   @override
-  Future<Bounds> getBounds({required bool resultTypeLatLng}) async {
+  Future<Bounds> getBounds({required bool shouldReturnLatLng}) async {
     String jsonStringResult =
-        await _controller.runJavaScriptReturningResult('map.getBounds()')
+        await _controller.runJavaScriptReturningResult(
+              'getBounds(${jsonEncode(_mapId)})',
+            )
             as String;
     if (Platform.isIOS) {
       jsonStringResult = NSDictionaryUtil.convert(jsonStringResult);
     }
     final Map<String, dynamic> json = jsonDecode(jsonStringResult);
     final Bounds bounds;
-    if (resultTypeLatLng) {
+    if (shouldReturnLatLng) {
       bounds = NLatLngBounds(
         southWest: NLatLng(json['_sw']['_lat'], json['_sw']['_lng']),
         northEast: NLatLng(json['_ne']['_lat'], json['_ne']['_lng']),
@@ -169,12 +298,14 @@ class NaverMapManager implements NaverMapManagerInterface {
 
   @override
   Future<void> setCenter({required Coord center}) async {
-    await _controller.runJavaScript('map.setCenter(${center.toJson()})');
+    await _controller.runJavaScript(
+      'window.setCenter(${jsonEncode(_mapId)}, ${center.toJson()})',
+    );
   }
 
   @override
   Future<void> setZoom({required int zoom}) async {
-    await _controller.runJavaScript('map.setZoom($zoom)');
+    await _controller.runJavaScript('setZoom(${jsonEncode(_mapId)}, $zoom)');
   }
 
   @override
@@ -183,7 +314,7 @@ class NaverMapManager implements NaverMapManagerInterface {
     required MarkerOptions markerOptions,
   }) async {
     await _controller.runJavaScript(
-      'window.addMarker($markerId, ${markerOptions.toJson()})',
+      'window.addMarker(${jsonEncode(_mapId)}, $markerId, ${markerOptions.toJson()})',
     );
   }
 
@@ -193,24 +324,30 @@ class NaverMapManager implements NaverMapManagerInterface {
     required MarkerOptions markerOptions,
   }) async {
     await _controller.runJavaScript(
-      'window.updateMarker($markerId, ${markerOptions.toJson()})',
+      'window.updateMarker(${jsonEncode(_mapId)}, $markerId, ${markerOptions.toJson()})',
     );
   }
 
   @override
   Future<void> removeMarker({required String markerId}) async {
-    await _controller.runJavaScript('window.removeMarker($markerId)');
+    await _controller.runJavaScript(
+      'window.removeMarker(${jsonEncode(_mapId)}, $markerId)',
+    );
   }
 
   @override
   Future<void> removeMarkerAll() async {
-    await _controller.runJavaScript('window.removeMarkerAll()');
+    await _controller.runJavaScript(
+      'window.removeMarkerAll(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<List<int>> getMarkerIds() async {
     String jsonStringResult =
-        await _controller.runJavaScriptReturningResult('window.getMarkerIds()')
+        await _controller.runJavaScriptReturningResult(
+              'window.getMarkerIds(${jsonEncode(_mapId)})',
+            )
             as String;
     if (Platform.isIOS) {
       jsonStringResult = NSDictionaryUtil.convert(jsonStringResult);
@@ -221,92 +358,118 @@ class NaverMapManager implements NaverMapManagerInterface {
 
   @override
   Future<void> addMarkerClickEvent({required String markerId}) async {
-    await _controller.runJavaScript('window.addMarkerClickEvent($markerId)');
+    await _controller.runJavaScript(
+      'window.addMarkerClickEvent(${jsonEncode(_mapId)}, $markerId)',
+    );
   }
 
   @override
   Future<void> removeMarkerClickEvent({required String markerId}) async {
-    await _controller.runJavaScript('window.removeMarkerClickEvent($markerId)');
+    await _controller.runJavaScript(
+      'window.removeMarkerClickEvent(${jsonEncode(_mapId)}, $markerId)',
+    );
   }
 
   @override
   Future<void> addMapClickEventListener() async {
-    await _controller.runJavaScript('window.addMapClickEventListener()');
+    await _controller.runJavaScript(
+      'window.addMapClickEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> removeMapClickEventListener() async {
-    await _controller.runJavaScript('window.removeMapClickEventListener()');
+    await _controller.runJavaScript(
+      'window.removeMapClickEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> addMapLongTapEventListener() async {
-    await _controller.runJavaScript('window.addMapLongTapEventListener()');
+    await _controller.runJavaScript(
+      'window.addMapLongTapEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> removeMapLongTapEventListener() async {
-    await _controller.runJavaScript('window.removeMapLongTapEventListener()');
+    await _controller.runJavaScript(
+      'window.removeMapLongTapEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> addMapIdleEventListener() async {
-    await _controller.runJavaScript('window.addMapIdleEventListener()');
+    await _controller.runJavaScript(
+      'window.addMapIdleEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> removeMapIdleEventListener() async {
-    await _controller.runJavaScript('window.removeMapIdleEventListener()');
+    await _controller.runJavaScript(
+      'window.removeMapIdleEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> addMapZoomChangedEventListener() async {
-    await _controller.runJavaScript('window.addMapZoomChangedEventListener()');
+    await _controller.runJavaScript(
+      'window.addMapZoomChangedEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> removeMapZoomChangedEventListener() async {
     await _controller.runJavaScript(
-      'window.removeMapZoomChangedEventListener()',
+      'window.removeMapZoomChangedEventListener(${jsonEncode(_mapId)})',
     );
   }
 
   @override
   Future<void> addMapZoomEndEventListener() async {
-    await _controller.runJavaScript('window.addMapZoomEndEventListener()');
+    await _controller.runJavaScript(
+      'window.addMapZoomEndEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> removeMapZoomEndEventListener() async {
-    await _controller.runJavaScript('window.removeMapZoomEndEventListener()');
+    await _controller.runJavaScript(
+      'window.removeMapZoomEndEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> addMapZoomStartEventListener() async {
-    await _controller.runJavaScript('window.addMapZoomStartEventListener()');
+    await _controller.runJavaScript(
+      'window.addMapZoomStartEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> removeMapZoomStartEventListener() async {
-    await _controller.runJavaScript('window.removeMapZoomStartEventListener()');
+    await _controller.runJavaScript(
+      'window.removeMapZoomStartEventListener(${jsonEncode(_mapId)})',
+    );
   }
 
   @override
   Future<void> addMapCenterChangedEventListener() async {
     await _controller.runJavaScript(
-      'window.addMapCenterChangedEventListener()',
+      'window.addMapCenterChangedEventListener(${jsonEncode(_mapId)})',
     );
   }
 
   @override
   Future<void> removeMapCenterChangedEventListener() async {
     await _controller.runJavaScript(
-      'window.removeMapCenterChangedEventListener()',
+      'window.removeMapCenterChangedEventListener(${jsonEncode(_mapId)})',
     );
   }
 
   @override
   Future<void> disposeMap() async {
-    await _controller.runJavaScript('window.disposeMap()');
+    await _controller.runJavaScript('window.disposeMap(${jsonEncode(_mapId)})');
   }
 }
--- lib/sdk_app/naver_map_widget_app.dart
+++ lib/sdk_app/naver_map_widget_app.dart
@@ -1,125 +1,35 @@
 part of 'naver_maps_sdk_flutter_app.dart';
 
 class NaverMapWidget extends StatelessWidget {
-  final MapOptions? _mapOptions;
-
+  final MapOptions? mapOptions;
   final NaverMapManager naverMapManager;
+  final bool showLoading;
 
   const NaverMapWidget({
     super.key,
     required this.naverMapManager,
-    MapOptions? mapOptions,
-  }) : _mapOptions = mapOptions;
+    this.mapOptions,
+    this.showLoading = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    _initializeWebViewController();
-    return WebViewWidget(controller: naverMapManager._controller);
-  }
+    return FutureBuilder<void>(
+      future: naverMapManager.onPageFinished,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return showLoading
+              ? const Center(child: CircularProgressIndicator())
+              : const SizedBox.shrink();
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
 
-  void _initializeWebViewController() async {
-    naverMapManager._controller
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            debugPrint('WebView is Loading Progress:: $progress');
-          },
-          onPageStarted: (String url) {
-            debugPrint('WebView Page Started:: $url');
-          },
-          onPageFinished: (String url) {
-            debugPrint('WebView Page Finished:: $url');
-            _initializeNaverMap(_mapOptions);
-          },
-          onWebResourceError: (WebResourceError error) {
-            debugPrint('WebView Error:: ${error.description}');
-          },
-        ),
-      )
-      ..addJavaScriptChannel(
-        'FlutterMethodChannel',
-        onMessageReceived: (JavaScriptMessage message) {
-          debugPrint('WebView Message:: ${message.message}');
-          try {
-            final Map<String, dynamic> data = jsonDecode(message.message);
-            final String type = data['type'];
+        naverMapManager._initializeNaverMap(mapOptions);
 
-            if (type == 'mapLoaded') {
-              naverMapManager.onMapLoadSuccess();
-            } else if (type == 'mapLoadFail') {
-              naverMapManager.onMapLoadFail();
-            }
-          } catch (e) {
-            naverMapManager.onMapLoadFail();
-          }
-        },
-      )
-      ..addJavaScriptChannel(
-        'MarkerEvent',
-        onMessageReceived: (JavaScriptMessage message) {
-          final Map<String, dynamic> json = jsonDecode(message.message);
-          if (json['type'] == 'markerClick') {
-            naverMapManager.onMarkerClick(json['markerId']);
-          }
-        },
-      )
-      ..addJavaScriptChannel(
-        'MapEvent',
-        onMessageReceived: (JavaScriptMessage message) {
-          final Map<String, dynamic> json = jsonDecode(message.message);
-          final String type = json['type'];
-          switch (type) {
-            case 'click':
-              naverMapManager.onMapClick(json['coord']);
-              break;
-            case 'longtap':
-              naverMapManager.onMapLongTap(json['coord']);
-              break;
-            case 'idle':
-              naverMapManager.onMapIdle();
-              break;
-            case 'zoom_changed':
-              naverMapManager.onMapZoomChanged(json['zoom']);
-              break;
-            case 'zoomend':
-              naverMapManager.onMapZoomEnd();
-              break;
-            case 'zoomstart':
-              naverMapManager.onMapZoomStart();
-              break;
-            case 'center_changed':
-              naverMapManager.onMapCenterChanged(json['center']);
-              break;
-          }
-        },
-      )
-      ..loadHtmlString(
-        await _initNaverScript(),
-        baseUrl: NaverMapSDK.instance.webServiceUrl,
-      );
-  }
-
-  Future<String> _initNaverScript() async {
-    final String beforeHtmlContent = await rootBundle.loadString(
-      'packages/naver_maps_sdk_flutter/assets/naver_map.html',
-    );
-
-    final String scriptPlaceHolder =
-        '<script id="naverMapScript" type="text/javascript"></script>';
-    final String scriptNaverWithClientId =
-        '<script type="text/javascript" src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=${NaverMapSDK.instance.clientId}&language=${NaverMapSDK.instance.language.value}"></script>';
-    return beforeHtmlContent.replaceFirst(
-      scriptPlaceHolder,
-      scriptNaverWithClientId,
-    );
-  }
-
-  Future<void> _initializeNaverMap(MapOptions? mapOptions) async {
-    debugPrint('mapOptions:: ${mapOptions?.toJson() ?? '{}'}');
-    await naverMapManager._controller.runJavaScript(
-      'initMap(${mapOptions?.toJson() ?? '{}'})',
+        return WebViewWidget(controller: naverMapManager._controller);
+      },
     );
   }
 }
